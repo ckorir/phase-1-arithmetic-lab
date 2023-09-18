2c6ab706aff8a6465a588e45b83f72ad07d77e57
ckorir <chrisruto.k@gmail.com>
2023-09-18 12:29:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+//multiply
+const num1 = 31;
+const num2 = 2;
+const num3 = 14;
+const num4 = 5;
+
+
+const multiply = num1 * num2;
+
+//random
+const random = Math.floor(Math.random() * 10) + 1;
+
+//mod
+const mod = num3 % num4;
+
+//max
+const max = Math.max(2, 4, 10, 20, 9);
